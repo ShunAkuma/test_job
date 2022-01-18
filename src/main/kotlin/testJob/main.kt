89aa9622ebddb32e6c8cfa0b2ad221139642b7aa
Hidden <wasbornunnamed@gmail.com>
2022-01-19 02:29:36 +0300

    add converter

--- src/main/kotlin/testJob/main.kt
+++ src/main/kotlin/testJob/main.kt
@@ -4,8 +4,13 @@ import testJob.reciever.Reciver
 
 fun main(){
 //    count id range (1-3)
-    val string : String = ("T1004, , T1005")
+    val string : String = ("T1004, , T1005,,,,    Y1002")
     val array: Array<String> = arrayOf("T1005, ,T1004", "T1008, ,T1004,,T1005","T1009, ,T1005")
+
+    println(string.length)
+    val a = Reciver()
+    println(a.findAllMatchInArray(string))
+
 
     println(Reciver.test(string, array))
 
--- src/main/kotlin/testJob/reciever/Reciver.kt
+++ src/main/kotlin/testJob/reciever/Reciver.kt
@@ -6,6 +6,9 @@ class Reciver() {
         @JvmStatic
         fun test(string : String, array : Array<String> ):ArrayList<String>{
 
+
+
+
             return arrayListOf()
 //            return listOf("hello") as ArrayList<String>
 //            Cast array to ArrayList<String>
@@ -13,6 +16,30 @@ class Reciver() {
         }
     }
 
+    fun converterStringToArrayList(string: String) : ArrayList<String>{
+        val arrayL: ArrayList<String> = arrayListOf()
+        var startIndex = -1
+        var endIndex = -1
+        val listSize = string.length - 1
+        string.forEachIndexed{_index, _element ->
+            if (_element.toLowerCase().toString() in "a" .. "z" ){
+                startIndex = _index
+            }
+            if ((startIndex != -1) && (_element.equals(',') || _element.equals(' ')) || listSize == _index){
+                endIndex = _index
+                if (listSize == _index){
+                    endIndex+=1
+                }
+
+                val temp = string.substring(startIndex, endIndex)
+                arrayL.add(temp)
+
+                startIndex = -1
+                endIndex = -1
+            }
+        }
+        return arrayL
+    }
 }
 
 
