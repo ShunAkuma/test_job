b620ff51fef2bff72a69d6ab8fa65fe66685e9fd
Hidden <wasbornunnamed@gmail.com>
2022-01-19 01:12:59 +0300

    added method and test fields

--- src/main/kotlin/testJob/main.kt
+++ src/main/kotlin/testJob/main.kt
@@ -1,2 +1,12 @@
 package testJob
 
+import testJob.reciever.Reciver
+
+fun main(){
+//    count id range (1-3)
+    val string : String = ("T1004, , T1005")
+    val array: Array<String> = arrayOf("T1005, ,T1004", "T1008, ,T1004,,T1005","T1009, ,T1005")
+
+    println(Reciver.test(string, array))
+
+}
---
+++ src/main/kotlin/testJob/reciever/Reciver.kt
@@ -0,0 +1,20 @@
+package testJob.reciever
+
+class Reciver() {
+
+    companion object{
+        @JvmStatic
+        fun test(string : String, array : Array<String> ):ArrayList<String>{
+
+            return arrayListOf()
+//            return listOf("hello") as ArrayList<String>
+//            Cast array to ArrayList<String>
+//            val list: ArrayList<String> = array.toCollection(ArrayList())
+        }
+    }
+
+}
+
+
+//Сатичный метод (строку и массив строк): ArrayList<>
+//
