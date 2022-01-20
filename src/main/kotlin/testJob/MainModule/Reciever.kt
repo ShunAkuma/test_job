e3db8615862ca64b0f73366cf94612a9177f2889
Hidden <wasbornunnamed@gmail.com>
2022-01-20 13:32:08 +0300

    fixed static method

--- src/main/kotlin/testJob/MainModule/Reciever.kt
+++ src/main/kotlin/testJob/MainModule/Reciever.kt
@@ -4,22 +4,16 @@ class Reciever() {
 
     companion object{
         fun staticMethod(string : String, array : Array<String> ):ArrayList<String>{
-            var finalList:ArrayList<String>
             val reciver = Reciever()
-
-            finalList = reciver.creatingFinalArray(reciver.converterStringToArrayList(string),array)
-
-            return finalList
+            return reciver.creatingFinalArray(reciver.converterStringToArrayList(string),array)
         }
     }
 
     fun converterStringToArrayList(string: String) : ArrayList<String>{
         val arrayList: ArrayList<String> = arrayListOf()
-
-
         var startIndex = -1
         var endIndex = -1
-        var listSize = string.length - 1
+        val listSize = string.length - 1
 
         string.forEachIndexed{ _index, _element ->
             if (_element.toLowerCase().toString() in "a" .. "z" ){
